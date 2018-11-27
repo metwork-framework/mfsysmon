534bd719651a4fbcbdeada0b02684e6429886bd8
thebaptiste <jean-baptiste.veslin@meteo.fr>
2018-11-27 11:17:17 +0100

    fix: fix user used

--- integration_tests/0001_test_mfsysmon_operational/test_mfsysmon_operational.sh
+++ integration_tests/0001_test_mfsysmon_operational/test_mfsysmon_operational.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Test if mfsysmon.start/status/stop are ok
-su --command="mfsysmon.start" - mfdata
+su --command="mfsysmon.start" - mfsysmon
 if test $? -ne 0; then
     echo "Test mfsysmon.start KO"
     exit 1
 else
     echo "Test mfsysmon.start OK"
 fi
-su --command="mfsysmon.status" - mfdata
+su --command="mfsysmon.status" - mfsysmon
 if test $? -ne 0; then
     echo "Test mfsysmon.status KO"
     exit 1
 else
     echo "Test mfsysmon.status OK"
 fi
-su --command="mfsysmon.stop" - mfdata
+su --command="mfsysmon.stop" - mfsysmon
 if test $? -ne 0; then
     echo "Test mfsysmon.stop KO"
     exit 1
