1e55c79f351c766ba1767168097fac7572df9747
thebaptiste <jean-baptiste.veslin@meteo.fr>
2019-01-21 15:53:01 +0100

    feat: execute integration tests directly from mfsysmon module

--- integration_tests/0001_test_mfsysmon_operational/test_mfsysmon_operational.sh
+++ integration_tests/0001_test_mfsysmon_operational/test_mfsysmon_operational.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
-# Test if mfsysmon.start/status/stop are ok
-su --command="mfsysmon.start" - mfsysmon
-if test $? -ne 0; then
-    echo "Test mfsysmon.start KO"
-    exit 1
-else
-    echo "Test mfsysmon.start OK"
-fi
-su --command="mfsysmon.status" - mfsysmon
+# Test if mfsysmon.status is ok
+mfsysmon.status
 if test $? -ne 0; then
     echo "Test mfsysmon.status KO"
     exit 1
 else
     echo "Test mfsysmon.status OK"
-fi
-su --command="mfsysmon.stop" - mfsysmon
-if test $? -ne 0; then
-    echo "Test mfsysmon.stop KO"
-    exit 1
-else
-    echo "Test mfsysmon.stop OK"
 fi
 exit 0
